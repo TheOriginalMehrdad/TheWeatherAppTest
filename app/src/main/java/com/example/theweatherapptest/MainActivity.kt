dfd488b354aa67fd23a75844d7e9c73c280a9869
Mehrdad S.F. <meddisommer@gmail.com>
2023-12-02 19:36:19 +0330

    Add network access in manifest , Commit

--- app/src/main/java/com/example/theweatherapptest/MainActivity.kt
+++ app/src/main/java/com/example/theweatherapptest/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.theweatherapptest.ui.theme.TheWeatherAppTestTheme
 
@@ -37,7 +38,12 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreenPreview() {
     TheWeatherAppTestTheme {
-        MainScreen()
+        Surface(
+            color = Color.White,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            MainScreen()
+        }
     }
 }
 
